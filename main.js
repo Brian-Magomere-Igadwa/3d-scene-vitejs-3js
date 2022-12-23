00f5dd8781f9df24fea6f7939b76670ba5035dca
Igadwa Brian <igadwabrian7@gmail.com>
2022-12-23 12:07:39 +0300

    #3 Combine Plane Mesh To A Box Mesh in 3D Space

--- main.js
+++ main.js
@@ -11,17 +11,32 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 
-console.log(scene);
-console.log(camera);
-console.log(renderer);
-
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
 const mesh = new THREE.Mesh(boxGeometry, material);
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+
 scene.add(mesh);
+scene.add(planeMesh);
 
 camera.position.z = 5;
-renderer.render(scene, camera);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  mesh.rotation.x += 0.01;
+  planeMesh.rotation.x += 0.01;
+
+  mesh.rotation.y += 0.01;
+  planeMesh.rotation.y += 0.01;
+}
+
+animate();
